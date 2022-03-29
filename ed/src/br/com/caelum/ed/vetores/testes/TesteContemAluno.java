d55d3dec8e9f4ad068f8d3f14254d80c09404691
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-03-28 21:52:59 -0300

    doing

---
+++ ed/src/br/com/caelum/ed/vetores/testes/TesteContemAluno.java
@@ -0,0 +1,28 @@
+package br.com.caelum.ed.vetores.testes;
+
+import br.com.caelum.ed.Aluno;
+import br.com.caelum.ed.vetores.Vetor;
+
+public class TesteContemAluno {
+
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno();
+		Aluno a2 = new Aluno();
+		
+		a1.setNome("Rafael");
+		a2.setNome("Paulo");
+		
+		Vetor lista = new Vetor();
+
+		lista.adiciona(a1);
+		lista.adiciona(a2);
+	
+		System.out.println(lista.contem(a1));
+		System.out.println(lista.contem(a2));
+	
+		Aluno a3 = new Aluno();
+		a3.setNome("Ana");
+		System.out.println(lista.contem(a3));
+	}
+	
+}
