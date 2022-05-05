6d10ee7702f6be353876f8c1ca866f94c7db8d14
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-05-04 22:10:19 -0300

    adiciona teste que pega por posicao

---
+++ ed/src/br/com/caelum/ed/listasligadas/TestePegaPorPosicao.java
@@ -0,0 +1,14 @@
+package br.com.caelum.ed.listasligadas;
+
+public class TestePegaPorPosicao {
+	public static void main(String[] args) {
+		ListaLigada lista = new ListaLigada();
+		
+		lista.adiciona("Rafael");
+		lista.adiciona("Paulo");
+
+		System.out.println(lista.pega(0));
+		System.out.println(lista.pega(1));
+	}
+	
+}
