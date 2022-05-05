e090fd06fa7a84b102b839344b347e69c25bd045
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-05-04 22:09:54 -0300

    adiciona teste que insere por posicao

---
+++ ed/src/br/com/caelum/ed/listasligadas/TesteAdicionaPorPosicao.java
@@ -0,0 +1,14 @@
+package br.com.caelum.ed.listasligadas;
+
+public class TesteAdicionaPorPosicao {
+	public static void main(String[] args) {
+		ListaLigada lista = new ListaLigada();
+		
+		lista.adiciona("Rafael");
+		lista.adiciona(0, "Paulo");
+		lista.adiciona(1, "Camila");
+
+		System.out.println(lista);
+	}
+	
+}
