33228f13cbb344f905f1f5f42c73fd3eb5732e13
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-05-04 22:08:22 -0300

    adiciona teste de tamanho

---
+++ ed/src/br/com/caelum/ed/listasligadas/TesteTamanho.java
@@ -0,0 +1,17 @@
+package br.com.caelum.ed.listasligadas;
+
+public class TesteTamanho {
+	public static void main(String[] args) {
+		ListaLigada lista = new ListaLigada();
+		
+		lista.adiciona("Rafael");
+		lista.adiciona("Paulo");
+
+		System.out.println(lista.tamanho());
+
+		lista.adiciona("Camila");
+
+		System.out.println(lista.tamanho());
+	}
+	
+}
