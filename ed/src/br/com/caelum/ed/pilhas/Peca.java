cc989754837a801e0fe3d48cd9b5787a18c1436e
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-05-30 21:39:04 -0300

    1) Implemente a classe Peca no pacote br.com.caelum.ed para poder criar objetos.

---
+++ ed/src/br/com/caelum/ed/pilhas/Peca.java
@@ -0,0 +1,15 @@
+package br.com.caelum.ed.pilhas;
+
+public class Peca {
+
+	private String nome;
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
