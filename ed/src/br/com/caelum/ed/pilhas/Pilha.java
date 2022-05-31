d34201ab28c77f568ef122e2015ae483e3680299
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-05-31 20:51:37 -0300

    2) Implemente a classe Pilha para peças vista neste capítulo. br.com.caelum.ed.pilhas

---
+++ ed/src/br/com/caelum/ed/pilhas/Pilha.java
@@ -0,0 +1,26 @@
+package br.com.caelum.ed.pilhas;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import br.com.caelum.ed.pilhas.Peca;
+
+public class Pilha {
+
+	// generics aqui aplcado
+	private List<Peca> pecas = new LinkedList<Peca>();
+	
+	public void insere(Peca peca) {
+		this.pecas.add(peca);
+	}
+
+	// lembre-se indices comecam em 0, entao é sempre tamanhoFisico - 1
+	public Peca remove() {	
+		return this.pecas.remove(this.pecas.size() - 1);
+	}
+	
+	public boolean vazia() {
+		return this.pecas.size() == 0;
+	}
+	
+}
