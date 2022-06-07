65758163435dc7e49c87e28b7f368caedf34ddfc
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-06 21:51:10 -0300

    5) (opcional) É possível implementar a nossa Pilha utilizando internamente uma ArrayList em vez de LinkedList ? Teremos algum ganho ou perda no consumo de tempo de alguma das operações?

--- ed/src/br/com/caelum/ed/pilhas/Pilha.java
+++ ed/src/br/com/caelum/ed/pilhas/Pilha.java
@@ -27,3 +27,14 @@ public class Pilha {
 	}
 	
 }
+/*
+*	5) (opcional) É possível implementar a nossa Pilha utilizando internamente uma ArrayList em vez de
+*	LinkedList ? Teremos algum ganho ou perda no consumo de tempo de alguma das operações?
+*
+*	R: Se trocar para arraylist teriamos perda de performance em inserção e remocao dos elementos.
+*	No caso o cenario de maior ganho do arraylist sob a linked list e ao percorrer a lista, 
+*	e na pilha o acesso e dado sempre pelo ultimo elemento, entao nao sendo aplicado essa operacao nesse cenario.
+*
+*
+*
+*/
