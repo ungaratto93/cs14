4475a9bfc39989ea8348271f25acfb097e9182d6
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-07 20:46:13 -0300

    add classe mensagem

---
+++ ed/src/br/com/caelum/ed/pilhas/Mensagem.java
@@ -0,0 +1,18 @@
+package br.com.caelum.ed.pilhas;
+
+public class Mensagem {
+
+	public String texto = "";
+	
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	public String getText() {
+		return this.texto;
+	}
+
+	public char getChartAt(int index) {
+		return this.texto.charAt(index);
+	}
+}
