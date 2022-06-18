0b9afbb65c5f4f36bd7c7be9c258eb7fce11f65c
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-17 22:11:49 -0300

    adiciona a classe para implementar o conjunto de palavras

---
+++ ed/src/br/com/caelum/ed/conjuntos/ConjuntoEspalhamento.java
@@ -0,0 +1,29 @@
+package br.com.caelum.ed.conjuntos;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ConjuntoEspalhamento {
+	
+	public void adiciona(String palavra) {
+		//implementação
+	}
+	
+	public void remove(String palavra) {
+		//implementação
+	}
+
+	public Boolean contem(String palavra) {
+		//implementação
+		return false;
+	}
+
+	public List<String> pegaTodas() {
+		//implementação
+		return new ArrayList<String>();
+	}
+	
+	public int tamanho() {
+		return 0;
+	}
+}
