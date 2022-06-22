e32d40b3bf4efe54fa125aa2f904a3d7da2742d9
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-21 21:32:48 -0300

    atualize teste tamanho

--- ed/src/br/com/caelum/ed/conjuntos/testes/TesteTamanho.java
+++ ed/src/br/com/caelum/ed/conjuntos/testes/TesteTamanho.java
@@ -8,5 +8,11 @@ public class TesteTamanho {
 		ConjuntoEspalhamento conjunto = new ConjuntoEspalhamento();
 		System.out.println("O tamanho do conjunto é 0 ? " + (conjunto.tamanho() == 0));		
 
+		conjunto.adiciona("jose");
+		conjunto.adiciona("jose");
+		conjunto.adiciona("jose");
+		System.out.println("O tamanho do conjunto é 1 ? " + (conjunto.tamanho() == 1));		
+
+		
 	}
 }
