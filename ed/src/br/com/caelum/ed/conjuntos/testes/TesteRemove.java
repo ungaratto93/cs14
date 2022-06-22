190747d46701a907b2649ad7338e0e8bb96fe793
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-21 21:11:14 -0300

    atualiza teste remove

--- ed/src/br/com/caelum/ed/conjuntos/testes/TesteRemove.java
+++ ed/src/br/com/caelum/ed/conjuntos/testes/TesteRemove.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ed.conjuntos.testes;
 
+import java.util.List;
+
 import br.com.caelum.ed.conjuntos.ConjuntoEspalhamento;
 
 public class TesteRemove {
@@ -12,10 +14,19 @@ public class TesteRemove {
 		conjunto.adiciona("apostila");
 		
 		System.out.println("O tamanho do conjunto é 3 ? " + (conjunto.tamanho() == 3));		
-
+		System.out.println("Lista de palavras antes de remover :");
+		List<String> palavras = conjunto.pegaTodas();
+		for (String palavra : palavras) {
+			System.out.println(palavra);
+		}
+		
 		conjunto.remove("apostila");
 
 		System.out.println("O tamanho do conjunto é 2 ? " + (conjunto.tamanho() == 2));		
-
+		System.out.println("Lista de palavras apos remover :");
+		palavras = conjunto.pegaTodas();
+		for (String palavra : palavras) {
+			System.out.println(palavra);
+		}
 	}
 }
