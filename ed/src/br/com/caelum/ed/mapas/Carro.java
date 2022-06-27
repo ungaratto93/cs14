9b0aba4d855e984265d2ad246259799e336275f5
ungaratto93 <eduardo.ungarattolima@gmail.com>
2022-06-27 20:56:25 -0300

    1) Implemente a classe Carro no pacote br.com.caelum.ed.

---
+++ ed/src/br/com/caelum/ed/mapas/Carro.java
@@ -0,0 +1,20 @@
+package br.com.caelum.ed.mapas;
+
+public class Carro {
+
+	private String nome;
+	
+	public Carro(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	@Override
+	public String toString() {
+		return "Carro: " + this.nome;
+	}
+
+}
